9f6959c65a34a4f6da67e959b81df0811ed57979
Derric <Derric_Lynns@comcast.com>
2019-06-19 23:11:05 +0530

    updated use of CMAKE_INSTALL_LIBDIR for temporary configure files
    	this is useful for builds set with
    	CMAKE_INSTALL_LIBDIR as / root folders like /usr/lib
    	modified:   cmake/install.cmake

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -103,7 +103,7 @@ endforeach()
 # Install configuration
 set(_cmakedir_desc "Directory relative to CMAKE_INSTALL to install the cmake configuration files")
 if(NOT MSVC)
-  set(CMAKE_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/protobuf" CACHE STRING "${_cmakedir_desc}")
+  set(CMAKE_INSTALL_CMAKEDIR "cmake/protobuf" CACHE STRING "${_cmakedir_desc}")
 else()
   set(CMAKE_INSTALL_CMAKEDIR "cmake" CACHE STRING "${_cmakedir_desc}")
 endif()
